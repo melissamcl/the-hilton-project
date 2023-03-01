29d5801112f7d7cb3b9ddced4617677e6bee6ed2
melissamcl <mcl.melissa.m@gmail.com>
2023-03-01 18:13:40 -0500

    committing before messing with webpack again to try to get map to display

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Form from './Form.js';
+import Map from './Map.js';
 
 const App = () => {
   return (
     <div>
       <Form />
+      {/* <Map /> */}
     </div>
   )
 }
