73f663b4c4ad8d7fe965cb2fcbf0c60411b1f1d7
melissamcl <mcl.melissa.m@gmail.com>
2023-05-28 13:56:42 -0400

    reorganization

---
+++ client/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div id="app">
+      <div id="title">
+        <h1>The Hilton Project</h1>
+      </div>
+
+      <div id="result"></div>
+    </div>
+  );
+};
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import App from './components/App.js'
+import App from './App.js';
 
-render(
-  <App />,
-  document.querySelector('#root')
-);
+render(<App />, document.querySelector('#root'));
